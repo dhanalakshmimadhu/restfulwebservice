1fa4e65a042ce56fd84c45fd3d7f85cd2a955a56
dhanalakshmimadhu <132139062+dhanalakshmimadhu@users.noreply.github.com>
2026-05-28 19:09:58 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,32 @@
+package com.test.rest;
+
+import java.util.List;
+
+public class UserService {
+	UserDAO userDao = new UserDAO();
+	 
+    public List<User> getAllUsers() {
+        List<User> userList = userDao.getAllUsers();
+        return userList;
+    }
+ 
+    public User getUserForId(String id) {
+        User user = userDao.getUserForId(id);
+        return user;
+    }
+ 
+    public User createUser(User user) {
+        User userResponse = userDao.createUser(user);
+        return userResponse;
+    }
+ 
+    public User updateUser(User user) {
+        User userResponse = userDao.updateUser(user);
+        return userResponse;
+    }
+ 
+    public User deleteUser(String id) {
+        User userResponse = userDao.deleteUser(id);
+        return userResponse;
+    }
+}
